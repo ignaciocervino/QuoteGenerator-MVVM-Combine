fa9bf6e6f3e223d89abe23d7e291ffc57d961397
ignaciocervino <ignaciocervino@gmail.com>
2023-04-01 18:04:08 -0300

    Setup QuoteService

---
+++ QuoteGenerator-MVVM-Combine/Model/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuoteGenerator-MVVM-Combine
+//
+//  Created by Ignacio Cervino on 01/04/2023.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    let content: String
+    let author: String
+}
---
+++ QuoteGenerator-MVVM-Combine/Service/QuoteService.swift
@@ -0,0 +1,32 @@
+//
+//  QuoteService.swift
+//  QuoteGenerator-MVVM-Combine
+//
+//  Created by Ignacio Cervino on 01/04/2023.
+//
+
+import Foundation
+import Combine
+
+private enum Constants {
+    static let endpointURL = "https://api.quotable.io/random"
+    static let domain = "ignacio.cervino.QuoteGenerator-MVVM-Combine"
+    static let invalidURL = "Invalid URL"
+}
+
+class QuoteService: QuoteServicing {
+    func getRandomQuote() -> AnyPublisher<Quote, Error> {
+        guard let url = URL(string: Constants.endpointURL) else {
+            let error = NSError(domain: Constants.domain, code: -1, userInfo: [NSLocalizedDescriptionKey: Constants.invalidURL])
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .catch { error in
+                return Fail(error: error).eraseToAnyPublisher()
+            }
+            .map({ $0.data })
+            .decode(type: Quote.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
