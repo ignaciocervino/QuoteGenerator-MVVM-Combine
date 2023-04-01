8e13f3ea6ec6e6314b4dbd656194a5232a48e3df
ignaciocervino <ignaciocervino@gmail.com>
2023-04-01 18:17:15 -0300

    setup protocol structure

---
+++ QuoteGenerator-MVVM-Combine/ViewModel/QuoteViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  QuoteViewModel.swift
+//  QuoteGenerator-MVVM-Combine
+//
+//  Created by Ignacio Cervino on 01/04/2023.
+//
+
+import Foundation
+
+class QuoteViewModel: QuoteViewModelProtocol {
+    
+}
---
+++ QuoteGenerator-MVVM-Combine/ViewModel/QuoteViewModelProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  QuoteViewModelProtocol.swift
+//  QuoteGenerator-MVVM-Combine
+//
+//  Created by Ignacio Cervino on 01/04/2023.
+//
+
+import Foundation
+
+protocol QuoteViewModelInputs {
+    
+}
+
+protocol QuoteViewModelOutputs {
+    
+}
+
+protocol QuoteViewModelProtocol: QuoteViewModelInputs, QuoteViewModelOutputs {}
